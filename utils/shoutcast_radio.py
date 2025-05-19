a0d9d5a856817a76c5022d03a4f0ad308db976ab
sangnt1552314 <sang1552314@gmail.com>
2025-05-19 23:05:08 +0700

    feat: refactor code

--- utils/shoutcast_radio.py
+++ utils/shoutcast_radio.py
@@ -15,132 +15,91 @@ TIMEOUT_DEFAULT = 5
 class ShoutcastRadio:
     def __init__(self, api_key = ''):
         self.api_key = api_key or os.getenv("SHOUTCAST_API_KEY")
+
         if not self.api_key:
             raise ValueError("Shoutcast API key is required")
-        
-    async def get_all_genres(self, **kwargs):
+
+    async def get_all_genres(self):
         """
-        Get the list of all genres. Supports both async and sync operations.
+        Get the list of all genres asynchronously.
         """
         params = {
             "k": self.api_key,
         }
-        async_request = kwargs.get("async_request", False)
 
-        if async_request:
-            async with httpx.AsyncClient() as client:
-                try:
-                    response = await client.get(f"{SHOUTCAST_BASE_URL}/legacy/genrelist", params=params, timeout=TIMEOUT_DEFAULT)
-                except httpx.TimeoutException:
-                    raise Exception("Request timed out. Please try again later.")
-        else:
-            try:
-                response = requests.get(f"{SHOUTCAST_BASE_URL}/legacy/genrelist", params=params, timeout=TIMEOUT_DEFAULT)
-            except requests.exceptions.Timeout:
-                raise Exception("Request timed out. Please try again later.")
+        response = await self._shoutcast_request_async("legacy/genrelist", params)
 
-        if response.status_code != 200:
-            raise Exception(f"Error fetching genres: {response.status_code} - {response.text}")
-        
-        xml_data = xmltodict.parse(response.content)
-        genre_list = xml_data.get("genrelist", {}).get("genre", {})
-        
-        if not genre_list:
-            return []
-        
-        return [{
-            "name": genre["@name"],
-            "count": genre["@count"],
-        } for genre in genre_list if isinstance(genre, dict)]
-    
+        return self._process_genres_response(response)
+
+    def get_all_genres_sync(self):
+        """
+        Get the list of all genres synchronously.
+        """
+        params = {
+            "k": self.api_key,
+        }
+
+        response = self._shoutcast_request_sync("legacy/genrelist", params)
+
+        return self._process_genres_response(response)
+
     async def get_primary_genres(self, **kwargs):
         """
-        Get the list of primary genres. Supports both async and sync operations.
+        Get the list of primary genres asynchronously.
         """
         params = {
             "k": self.api_key,
             "f": kwargs.get("format", "json"),
         }
-        async_request = kwargs.get("async_request", False)
 
-        if async_request:
-            async with httpx.AsyncClient() as client:
-                try:
-                    response = await client.get(f"{SHOUTCAST_BASE_URL}/genre/primary", params=params, timeout=TIMEOUT_DEFAULT)
-                except httpx.TimeoutException:
-                    raise Exception("Request timed out. Please try again later.")
-        else:
-            try:
-                response = requests.get(f"{SHOUTCAST_BASE_URL}/genre/primary", params=params, timeout=TIMEOUT_DEFAULT)
-            except requests.exceptions.Timeout:
-                raise Exception("Request timed out. Please try again later.")
+        response = await self._shoutcast_request_async("genre/primary", params)
 
-        if response.status_code != 200:
-            raise Exception(f"Error fetching primary genres: {response.status_code} - {response.text}")
+        return self._process_primary_genres_response(response)
 
-        data = response.json()
-        genre_list = data.get("response", {}).get("data", {}).get("genrelist", {}).get("genre", {})
+    def get_primary_genres_sync(self, **kwargs):
+        """
+        Get the list of all genres synchronously.
+        """
+        params = {
+            "k": self.api_key,
+            "f": kwargs.get("format", "json"),
+        }
 
-        if not genre_list:
-            return []
+        response = self._shoutcast_request_sync("genre/primary", params)
 
-        return [
-            {
-                "id": genre["id"],
-                "name": genre["name"],
-                "count": genre.get("count", 0),
-                "haschildren": genre["haschildren"],
-                "parentid": genre.get("parentid", 0),
-            }
-            for genre in genre_list
-        ]
-    
+        return self._process_primary_genres_response(response)
+
     async def get_secondary_genres(self, **kwargs):
         """
-        Get the list of secondary genres. Supports both async and sync operations.
+        Get the list of secondary genres asynchronously.
         """
         params = {
             "k": self.api_key,
             "f": kwargs.get("format", "json"),
             "parentid": kwargs.get("parentid", ""),
         }
-        async_request = kwargs.get("async_request", False)
 
-        if async_request:
-            async with httpx.AsyncClient() as client:
-                try:
-                    response = await client.get(f"{SHOUTCAST_BASE_URL}/genre/secondary", params=params, timeout=TIMEOUT_DEFAULT)
-                except httpx.TimeoutException:
-                    raise Exception("Request timed out. Please try again later.")
-        else:
-            try:
-                response = requests.get(f"{SHOUTCAST_BASE_URL}/genre/secondary", params=params, timeout=TIMEOUT_DEFAULT)
-            except requests.exceptions.Timeout:
-                raise Exception("Request timed out. Please try again later.")
+        response = await self._shoutcast_request_async("genre/secondary", params)
 
-        if response.status_code != 200:
-            raise Exception(f"Error fetching secondary genres: {response.status_code} - {response.text}")
-
-        data = response.json()
-        genre_list = data.get("response", {}).get("data", {}).get("genrelist", {}).get("genre", {})
-
-        if not genre_list:
-            return []
-
-        return [
-            {
-                "id": genre["id"],
-                "name": genre["name"],
-                "count": genre.get("count", 0),
-                "haschildren": genre["haschildren"],
-                "parentid": genre.get("parentid", 0),
-            }
-            for genre in genre_list
-        ]
+        return self._process_primary_genres_response(response)
     
+    def get_secondary_genres_sync(self, **kwargs):
+        """
+        Get the list of secondary genres synchronously.
+        """
+        params = {
+            "k": self.api_key,
+            "f": kwargs.get("format", "json"),
+            "parentid": kwargs.get("parentid", ""),
+        }
+
+        response = self._shoutcast_request_sync("genre/secondary", params)
+
+        return self._process_primary_genres_response(response)
+
     async def get_top_500_stations(self, **kwargs):
         """
-        Get the list of top 500 stations. Supports both async and sync operations.
+        Get the list of top 500 stations asynchronously.
         """
         params = {
             "k": self.api_key,
@@ -148,85 +107,61 @@ class ShoutcastRadio:
             "mt": kwargs.get("mt", "audio/mpeg"),
             "f": kwargs.get("format", "json"),
         }
-        async_request = kwargs.get("async_request", False)
 
-        if async_request:
-            async with httpx.AsyncClient() as client:
-                try:
-                    response = await client.get(f"{SHOUTCAST_BASE_URL}/legacy/Top500", params=params, timeout=TIMEOUT_DEFAULT)
-                except httpx.TimeoutException:
-                    raise Exception("Request timed out. Please try again later.")
-        else:
-            try:
-                response = requests.get(f"{SHOUTCAST_BASE_URL}/legacy/Top500", params=params, timeout=TIMEOUT_DEFAULT)
-            except requests.exceptions.Timeout:
-                raise Exception("Request timed out. Please try again later.")
+        response = await self._shoutcast_request_async("legacy/Top500", params)
 
-        if response.status_code != 200:
-            raise Exception(f"Error fetching top stations: {response.status_code} - {response.text}")
+        return self._process_top_stations_response(response)
+    
+    def get_top_500_stations_sync(self, **kwargs):
+        """
+        Get the list of top 500 stations synchronously.
+        """
+        params = {
+            "k": self.api_key,
+            "limit": kwargs.get("limit", (50, 0)),
+            "mt": kwargs.get("mt", "audio/mpeg"),
+            "f": kwargs.get("format", "json"),
+        }
 
-        xml_data = xmltodict.parse(response.content)
-        # tunin = xml_data.get("stationlist", {}).get("tunein", {})
-        stations = xml_data.get("stationlist", {}).get("station", {})
+        response = self._shoutcast_request_sync("legacy/Top500", params)
 
-        if not stations:
-            return []
-
-        return [
-            {
-                "id": str(station["@id"]),
-                "name": f"""{station["@name"]}""",
-                "value": str(station["@id"]),
-                "genre": station["@genre"],
-                # "stream_url": self.get_station_stream_url(station["@id"], tunin),
-            }
-            for station in stations
-        ]
+        return self._process_top_stations_response(response)
 
     async def get_now_playing_stations(self, **kwargs):
         """
-        Get the list of now playing stations. Supports both async and sync operations.
+        Get the list of now playing stations asynchronously.
         """
         params = {
             "ct": kwargs.get("ct", ""),
             "k": self.api_key,
-            "limit": kwargs.get("limit", 10),
+            "limit": kwargs.get("limit", (20, 0)),
             "mt": kwargs.get("mt", "audio/mpeg"),
             "f": kwargs.get("format", "json"),
         }
-        async_request = kwargs.get("async_request", False)
 
-        # Handle both async and sync requests
-        if async_request:
-            async with httpx.AsyncClient() as client:
-                response = await client.get(f"{SHOUTCAST_BASE_URL}/station/nowplaying", params=params, timeout=TIMEOUT_DEFAULT)
-        else:
-            response = requests.get(f"{SHOUTCAST_BASE_URL}/station/nowplaying", params=params, timeout=TIMEOUT_DEFAULT)
+        response = await self._shoutcast_request_async("station/nowplaying", params)
 
-        if response.status_code != 200:
-            raise Exception(f"Error fetching now playing stations: {response.status_code} - {response.text}")
+        return self._process_station_response(response)
+    
+    def get_now_playing_stations_sync(self, **kwargs):
+        """
+        Get the list of now playing stations synchronously.
+        """
+        params = {
+            "ct": kwargs.get("ct", ""),
+            "k": self.api_key,
+            "limit": kwargs.get("limit", (20, 0)),
+            "mt": kwargs.get("mt", "audio/mpeg"),
+            "f": kwargs.get("format", "json"),
+        }
 
-        data = response.json()
-        # tunin = data.get("response", {}).get("data", {}).get("stationlist", {}).get("tunein", {})
-        stations = data.get("response", {}).get("data", {}).get("stationlist", {}).get("station", {})
+        response = self._shoutcast_request_sync("station/nowplaying", params)
 
-        if not stations:
-            return []
-
-        return [
-            {
-                "id": str(station["id"]),
-                "name": f"""{station["name"]} - {station['genre']}""",
-                "value": str(station["id"]),
-                "genre": station["genre"],
-                # "stream_url": self.get_station_stream_url(station["id"], tunin),
-            }
-            for station in stations
-        ]
+        return self._process_station_response(response)
     
     async def get_stations_by_genre_or_bitrate(self, **kwargs):
         """
-        Get the list of stations by genre or bitrate. Supports both async and sync operations.
+        Get the list of stations by genre or bitrate asynchronously.
         """
         params = {
             "k": self.api_key,
@@ -236,33 +171,27 @@ class ShoutcastRadio:
             "br": kwargs.get("br", "128"),
             "genre_id": kwargs.get("genre_id", ""),
         }
-        async_request = kwargs.get("async_request", False)
 
-        if async_request:
-            async with httpx.AsyncClient() as client:
-                response = await client.get(f"{SHOUTCAST_BASE_URL}/station/advancedsearch", params=params, timeout=TIMEOUT_DEFAULT)
-        else:
-            response = requests.get(f"{SHOUTCAST_BASE_URL}/station/advancedsearch", params=params, timeout=TIMEOUT_DEFAULT)
+        response = await self._shoutcast_request_async("station/advancedsearch", params)
 
-        if response.status_code != 200:
-            raise Exception(f"Error fetching stations: {response.status_code} - {response.text}")
+        return self._process_station_response(response)
+    
+    def get_stations_by_genre_or_bitrate_sync(self, **kwargs):
+        """
+        Get the list of stations by genre or bitrate synchronously.
+        """
+        params = {
+            "k": self.api_key,
+            "limit": kwargs.get("limit", (20, 0)),
+            "mt": kwargs.get("mt", "audio/mpeg"),
+            "f": kwargs.get("format", "json"),
+            "br": kwargs.get("br", "128"),
+            "genre_id": kwargs.get("genre_id", ""),
+        }
 
-        data = response.json()
-        stations = data.get("response", {}).get("data", {}).get("stationlist", {}).get("station", {})
+        response = self._shoutcast_request_sync("station/advancedsearch", params)
 
-        if not stations:
-            return []
-
-        return [
-            {
-                "id": str(station["id"]),
-                "name": f"""{station["name"]} - {station['genre']}""",
-                "value": str(station["id"]),
-                "genre": station["genre"],
-                # "stream_url": self.get_station_stream_url(station["id"], tunin),
-            }
-            for station in stations
-        ]
+        return self._process_station_response(response)
 
     def get_station_stream_url(self, station_id="", tunin = {}):
         """
@@ -303,10 +232,128 @@ class ShoutcastRadio:
 
         return stream_url
 
+    def _shoutcast_request_sync(self, endpoint, params):
+        """
+        Helper function to make a GET request to the Shoutcast API synchronously.
+        """
+        try:
+            response = requests.get(f"{SHOUTCAST_BASE_URL}/{endpoint}", params=params, timeout=TIMEOUT_DEFAULT)
+            response.raise_for_status()
+        except requests.exceptions.Timeout:
+            raise Exception("Request timed out. Please try again later.")
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"Error fetching data: {e}")
+        except Exception as e:
+            raise Exception(f"An unexpected error occurred: {e}")
+
+        if response.status_code != 200:
+            raise Exception(f"Error fetching data: {response.status_code} - {response.text} - {endpoint} - {params}")
+
+        return response
+
+    async def _shoutcast_request_async(self, endpoint, params):
+        """
+        Helper function to make a GET request to the Shoutcast API asynchronously.
+        """
+        async with httpx.AsyncClient() as client:
+            try:
+                response = await client.get(f"{SHOUTCAST_BASE_URL}/{endpoint}", params=params, timeout=TIMEOUT_DEFAULT)
+                response.raise_for_status()
+            except httpx.TimeoutException:
+                raise Exception("Request timed out. Please try again later.")
+            except httpx.RequestError as e:
+                raise Exception(f"Error fetching data: {e}")
+            except Exception as e:
+                raise Exception(f"An unexpected error occurred: {e}")
+
+        if response.status_code != 200:
+            raise Exception(f"Error fetching data: {response.status_code} - {response.text} - {endpoint} - {params}")
+
+        return response
+
+    def _process_genres_response(self, response):
+        """
+        Helper method to process the genres response.
+        """
+        xml_data = xmltodict.parse(response.content)
+        genre_list = xml_data.get("genrelist", {}).get("genre", {})
+
+        if not genre_list:
+            return []
+
+        return [{
+            "name": genre["@name"],
+            "count": genre["@count"],
+        } for genre in genre_list if isinstance(genre, dict)]
+
+    def _process_primary_genres_response(self, response):
+        """
+        Helper method to process the genres response.
+        """
+        data = response.json()
+        genre_list = data.get("response", {}).get("data", {}).get("genrelist", {}).get("genre", {})
+
+        if not genre_list:
+            return []
+
+        return [
+            {
+                "id": genre["id"],
+                "name": genre["name"],
+                "count": genre.get("count", 0),
+                "haschildren": genre["haschildren"],
+                "parentid": genre.get("parentid", 0),
+            }
+            for genre in genre_list
+        ]
+
+    def _process_top_stations_response(self, response):
+        xml_data = xmltodict.parse(response.content)
+        stations = xml_data.get("stationlist", {}).get("station", {})
+
+        if not stations:
+            return []
+
+        return [
+            {
+                "id": str(station["@id"]),
+                "name": f"""{station["@name"]}""",
+                "value": str(station["@id"]),
+                "genre": station["@genre"],
+            }
+            for station in stations
+        ]
+
+    def _process_station_response(self, response):
+        """
+        Helper method to process the station response.
+        """
+        data = response.json()
+        stations = data.get("response", {}).get("data", {}).get("stationlist", {}).get("station", {})
+
+        if not stations:
+            return []
+
+        return [
+            {
+                "id": str(station["id"]),
+                "name": f"""{station["name"]} - {station['genre']}""",
+                "value": str(station["id"]),
+                "genre": station["genre"],
+            }
+            for station in stations
+        ]
+
 async def main():
+    # Example usage
     radio = ShoutcastRadio()
-    stations = await radio.get_secondary_genres(async_request=True, parentid=1)
+    genres = await radio.get_stations_by_genre_or_bitrate(genre_id="1")
+    print("Asynchronous Call - Genres:", genres)
 
 if __name__ == "__main__":
     # Run example
+    radio = ShoutcastRadio()
+    genres = radio.get_stations_by_genre_or_bitrate_sync(genre_id="1")
+    print("Synchronous Call - Genres:", genres)
+
     asyncio.run(main())
